11aa4a9ca078a511ba84dbe47fb6a79e105a7929
Pavel Shastin <pavelshastin@yandex.ru>
2018-10-22 08:00:28 +0300

    new one

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Router, Switch, Route} from 'react-router-dom';
-import {Provider} from 'react-redux';
-import createHistory from 'history/createBrowserHistory';
-import App from '/components/App';
-
-const history = createHistory();
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PageHeader from './components/Header'
 
 
-ReactDOM.render(<Provider store="store">
-	<Router history={history}>
-		<Switch>
-			<Route path="/" component={App}/>
-		</Switch>		
-	</Router>
-</Provider>, document.getElementById('root'))
+class App extends React.Component {
+	render() {
+		return (
+			<PageHeader />
+		)
+	}
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import {Router, Switch, Route} from 'react-router-dom';
+// import {Provider} from 'react-redux';
+// import createHistory from 'history/createBrowserHistory';
+// import App from '/components/App';
+
+// const history = createHistory();
+
+
+// ReactDOM.render(<Provider store="store">
+// 	<Router history={history}>
+// 		<Switch>
+// 			<Route path="/" component={App}/>
+// 		</Switch>		
+// 	</Router>
+// </Provider>, document.getElementById('root'))
